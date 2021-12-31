57628874c50d44353c1ebc4497ba4a13c0e7cba9
mark <markpackham1@gmail.com>
2021-12-31 16:59:23 +0000

    check for accessibility issues
    https://youtu.be/MO4vEAu3hKE?t=1470

--- public/js/main.js
+++ public/js/main.js
@@ -7,9 +7,30 @@ const testAccessibility = async (e) => {
     alert("Please add a url");
   } else {
     setLoading();
+
+    const response = await fetch(`/api/test?url=${url}`);
+
+    if (response.status !== 200) {
+      setLoading(false);
+      alert("Something went wrong");
+    } else {
+      // issues part taken from "data"
+      const { issues } = await response.json();
+      addIssuesToDOM(issues);
+      setLoading(false);
+    }
   }
 };
 // Add issues to the DOM
+const addIssuesToDOM = (issues) => {
+  const issuesOutput = document.querySelector("#issues");
+  issuesOutput.innerHTML = "";
+
+  if (issues.length === 0) {
+    issuesOutput.innerHTML = "<h4>No issues found</h4>";
+  } else {
+  }
+};
 
 // Set loading state (spinner)
 const setLoading = (isLoading = true) => {
@@ -22,5 +43,7 @@ const setLoading = (isLoading = true) => {
 };
 
 // Escape HTML (don't render the html tags)
+
+//Clear results
 
 document.querySelector("#form").addEventListener("submit", testAccessibility);
