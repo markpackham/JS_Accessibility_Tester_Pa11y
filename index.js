9f6ed61b0763da950f379f676a6f1ba5a6996443
mark <markpackham1@gmail.com>
2021-12-31 15:20:45 +0000

    add results reponse
    https://youtu.be/MO4vEAu3hKE?t=571

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.get("/api/test", (req, res) => {
+app.get("/api/test", async (req, res) => {
   if (!req.query.url) {
     res.status(400).json({ error: "url is required" });
   } else {
+    const results = await pa11y(req.query.url);
+    res.status(200).json(results);
   }
 });
 
