3d4b17ebe77ec56a97e91069d0bee9a4864cc30c
mark <markpackham1@gmail.com>
2021-12-31 16:40:52 +0000

    activate loading spinner
    https://youtu.be/MO4vEAu3hKE?t=1250

--- public/js/main.js
+++ public/js/main.js
@@ -3,11 +3,23 @@ const testAccessibility = async (e) => {
   e.preventDefault();
 
   const url = document.querySelector("#url").value;
-  console.log(url);
+  if (url === "") {
+    alert("Please add a url");
+  } else {
+    setLoading();
+  }
 };
 // Add issues to the DOM
 
 // Set loading state (spinner)
+const setLoading = (isLoading = true) => {
+  const loader = document.querySelector(".loader");
+  if (isLoading) {
+    loader.style.display = "block";
+  } else {
+    loader.style.display = "none";
+  }
+};
 
 // Escape HTML (don't render the html tags)
 
